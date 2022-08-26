fef4ec7c16fb0a611a916e5b73f1adbfe4a06f47
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2022-08-26 17:34:31 -0300

    added some comments and corrections

--- Atividade 1/ex2/client.py
+++ Atividade 1/ex2/client.py
@@ -1,17 +1,17 @@
 from socket import *
 
 serverIP = '192.168.1.106'  # localhost or your server IP address
-serverPort = 4567		# use the port number you wish (higher than 1023)
+serverPort = 4567		    # use the port number you wish (higher than 1023)
 
-clientSocket = socket(AF_INET, SOCK_STREAM)  # creates a socket (client side)
+clientSocket = socket(AF_INET, SOCK_STREAM) # creates a socket (client side)
 clientSocket.connect((serverIP, serverPort))
 message = input('Enter your lower-case word: ')
 encodedMessage = message.encode()
-clientSocket.send(encodedMessage) 
+clientSocket.send(encodedMessage)           # sends the message to server
 
 modifiedMessage = clientSocket.recv(1500)
 decodedMessage = modifiedMessage.decode()
 
-print(decodedMessage)   # print received/decoded message
+print(decodedMessage)           # print received/decoded message
 
 clientSocket.close()
